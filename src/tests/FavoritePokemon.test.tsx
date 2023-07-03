26ea1a8e5ab796b1824845dc6c515462adf07dcd
fernandoallisson <fernando.alisson@ufpe.br>
2023-07-03 18:19:56 -0300

    Configurando o teste para o componente FavoritePokemon

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,35 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe('Testando o componente FvoritePokemon.tsx', () => {
+  test('É exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito.', () => {
+    render(<FavoritePokemon pokemonList={ [] } />);
+    const message = screen.getByText('No favorite Pokémon found');
+    expect(message).toBeInTheDocument();
+  });
+  // test('Apenas são exibidos os Pokémon favoritados, caso seja favoritado algum.', () => {
+  //   const pokemonList = [
+  //     {
+  //       id: 143,
+  //       name: 'Snorlax',
+  //       type: 'Normal',
+  //       averageWeight: {
+  //         value: '460.0',
+  //         measurementUnit: 'kg',
+  //       },
+  //       image: 'https://archives.bulbagarden.net/media/upload/4/40/Spr_5b_143.png',
+  //       moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)',
+  //       foundAt: [
+  //         {
+  //           location: 'Kanto Vermillion City',
+  //           map: 'https://archives.bulbagarden.net/media/upload/5/54/Kanto_Vermilion_City_Map.png',
+  //         },
+  //       ],
+  //       summary: 'What sounds like its cry may actually be its snores or the rumblings of its hungry belly.',
+  //     },
+  //   ];
+  //   render(<FavoritePokemon pokemonList={ pokemonList } />);
+  //   const pokemonNames = screen.getAllByText(/Snorlax/i);
+  //   expect(pokemonNames.some((name) => name.textContent === 'Snorlax')).toBeTruthy();
+  // });
+});
